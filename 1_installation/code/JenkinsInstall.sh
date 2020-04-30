bfff32baff9b11412f3846c0877ba696e1767658
Anju <anjurose.d@gmail.com>
2020-04-30 09:44:32 +0000

    Added installation task

---
+++ 1_installation/code/JenkinsInstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install -y default-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins
