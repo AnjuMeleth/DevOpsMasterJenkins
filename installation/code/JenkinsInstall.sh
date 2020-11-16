ce3267222987f09c10c7544eb03ed29ccdd71f23
AnjuMeleth <46922680+AnjuMeleth@users.noreply.github.com>
2020-11-16 05:36:00 +0530

    Update JenkinsInstall.sh

--- installation/code/JenkinsInstall.sh
+++ installation/code/JenkinsInstall.sh
@@ -2,9 +2,9 @@
 sudo apt-get update
 sudo apt install -y default-jdk
 //wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+wget -q -O - http://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 //sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
